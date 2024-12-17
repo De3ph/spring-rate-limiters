869c9196c175076e81eaf320551d06de3e781ab0
De3ph <05190000108@ogrenci.ege.edu.tr>
2024-12-17 17:04:12 +0300

    feat: init project

---
+++ src/main/java/com/example/springratelimiters/SpringRateLimitersApplication.java
@@ -0,0 +1,13 @@
+package com.example.springratelimiters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRateLimitersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRateLimitersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springratelimiters/SpringRateLimitersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springratelimiters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRateLimitersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
